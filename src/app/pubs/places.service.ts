3c30389e8e6644f56f62cd3f76888bbdc16b54f4
mihai10001 <rvivits@gmail.com>
2021-01-07 17:01:20 +0200

    change place model, add opendays model

--- src/app/pubs/places.service.ts
+++ src/app/pubs/places.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Place } from './place.model';
+import { OpenDays } from './openDays.model';
 
 @Injectable({
   providedIn: 'root'
@@ -7,51 +8,59 @@ import { Place } from './place.model';
 export class PlacesService {
   private _places: Place[] = [
     new Place(
-      'p1',
       'Scart',
       'cool place, might come back later',
       'https://d2fdt3nym3n14p.cloudfront.net/venue/2107/gallery/5309/conversions/scart-loc-lejer-13866-big.jpg',
-      ['luni-vineri: 09:00-23:00', 'sambata: 11:00-23:00', 'duminica: 14:00-23:00'],
-      new Date(),
-      new Date()
+      new OpenDays(true, false, false),
+      4,
+      5,
+      5,
+      [[0, 0], [0, 1]]
     ),
+
     new Place(
-      'p2',
       '80`sPub',
       'same',
       'https://timisoara.fest.ro/files/places/8/image_884_2_preview.jpg',
-      ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-      new Date(),
-      new Date()
+      new OpenDays(true, false, false),
+      4,
+      5,
+      5,
+      [[0, 0], [0, 1]]
     ),
+
     new Place(
-      'p3',
       'D`arc',
       'also on lake',
       'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcScfa8GR2gbK3gczQjCz0V47zue29V0mHZ5Qg&usqp=CAU',
-      ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-      new Date(),
-      new Date()
-  ),
-  new Place(
-    'p4',
-    'Aethernative',
-    'downstairs',
-    'https://www.thebohoguide.com/wp-content/uploads/job-manager-uploads/main_image/2017/03/boho1.jpg',
-    ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-    new Date(),
-    new Date()
-  ),
-  new Place(
-    'p5',
-    'Cuib D`Arte',
-    'upstairs',
-    'https://media-cdn.tripadvisor.com/media/photo-s/19/ed/90/90/img-20191104-220623-373.jpg',
-    ['luni-joi: 10:00-02:00', 'vineri: 10:00-05:00' ,'sambata: 10:00-05:00', 'duminica: 10:00-02:00'],
-    new Date(),
-    new Date()
-  ),
+      new OpenDays(true, false, false),
+      4,
+      5,
+      5,
+      [[0, 0], [0, 1]]
+    ),
 
+    new Place(
+      'Aethernative',
+      'downstairs',
+      'https://www.thebohoguide.com/wp-content/uploads/job-manager-uploads/main_image/2017/03/boho1.jpg',
+      new OpenDays(true, false, false),
+      4,
+      5,
+      5,
+      [[0, 0], [0, 1]]
+    ),
+
+    new Place(
+      'Cuib D`Arte',
+      'upstairs',
+      'https://media-cdn.tripadvisor.com/media/photo-s/19/ed/90/90/img-20191104-220623-373.jpg',
+      new OpenDays(true, false, false),
+      4,
+      5,
+      5,
+      [[0, 0], [0, 1]]
+    ),
   ];
 
   get places(){
