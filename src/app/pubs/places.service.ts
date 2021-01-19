57d342d67fdb7398cabd064bc575b6668fa4fc30
mihai10001 <rvivits@gmail.com>
2021-01-19 14:27:16 +0200

    create places service with firestore, move hardcoded info on db

--- src/app/pubs/places.service.ts
+++ src/app/pubs/places.service.ts
@@ -1,75 +1,53 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 import { Place } from './place.model';
-import { OpenDays } from './openDays.model';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlacesService {
-  private _places: Place[] = [
-    new Place(
-      'Scart',
-      'cool place, might come back later',
-      'https://d2fdt3nym3n14p.cloudfront.net/venue/2107/gallery/5309/conversions/scart-loc-lejer-13866-big.jpg',
-      new OpenDays(true, false, false),
-      4,
-      5,
-      5,
-      [[0, 0], [0, 1]]
-    ),
+  allPlaces: BehaviorSubject<Place[] | null> = new BehaviorSubject<Place[] | null>(null);
 
-    new Place(
-      '80`sPub',
-      'same',
-      'https://timisoara.fest.ro/files/places/8/image_884_2_preview.jpg',
-      new OpenDays(true, false, false),
-      4,
-      5,
-      5,
-      [[0, 0], [0, 1]]
-    ),
+  constructor(private firestore: AngularFirestore) {
+    let getSubscription = this.getPlacesAPIObservable();
 
-    new Place(
-      'D`arc',
-      'also on lake',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcScfa8GR2gbK3gczQjCz0V47zue29V0mHZ5Qg&usqp=CAU',
-      new OpenDays(true, false, false),
-      4,
-      5,
-      5,
-      [[0, 0], [0, 1]]
-    ),
-
-    new Place(
-      'Aethernative',
-      'downstairs',
-      'https://www.thebohoguide.com/wp-content/uploads/job-manager-uploads/main_image/2017/03/boho1.jpg',
-      new OpenDays(true, false, false),
-      4,
-      5,
-      5,
-      [[0, 0], [0, 1]]
-    ),
-
-    new Place(
-      'Cuib D`Arte',
-      'upstairs',
-      'https://media-cdn.tripadvisor.com/media/photo-s/19/ed/90/90/img-20191104-220623-373.jpg',
-      new OpenDays(true, false, false),
-      4,
-      5,
-      5,
-      [[0, 0], [0, 1]]
-    ),
-  ];
-
-  get places(){
-    return [...this._places]; //return a copy of the array in order to directly edit the places 
+    getSubscription.subscribe(data => {
+      this.allPlaces.next(data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            ...(e.payload.doc.data() as {})
+          } as Place;
+        }));
+    });
   }
 
-  constructor() { }
-
-  getPlace(id: string){
-    return {...this._places.find(p => p.id === id)}; //clone the entire object
+  getPlacesAPIObservable() {
+    return this.firestore.collection('places').snapshotChanges();
   }
+
+  getPlaceAPIObservable(id: string) {
+    return this.firestore.collection('places').doc(id).get();
+  }
+
+  createPlaceAPIObservable(place: Place) {
+    // Normalise model for Firebase
+    delete place.id;
+    place = {...place};
+    place.openDays = {...place.openDays};
+
+    return this.firestore.collection('places').add(place);
+  }
+
+  updatePlaceAPIObservable(place: Place) {
+    // Normalise model for Firebase
+    delete place.id;
+    place = {...place};
+    place.openDays = {...place.openDays};
+
+    this.firestore.doc('places/' + place.id).update(place);
+  }
+
+  deletePlace() {}
 }
