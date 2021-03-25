ac1ec4023fca4809d6f9a70eb8337d52d24c7444
mihai10001 <rvivits@gmail.com>
2021-03-25 12:27:24 +0200

    update and implement discover component (JS)

--- src/app/pubs/discover/discover.page.ts
+++ src/app/pubs/discover/discover.page.ts
@@ -2,7 +2,10 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
-import { SegmentChangeEventDetail } from '@ionic/core';
+// import { SegmentChangeEventDetail } from '@ionic/core';
+import { FavouritePlacesService } from '../favourite-places.service';
+import { AuthService } from '../../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-discover',
@@ -10,22 +13,78 @@ import { SegmentChangeEventDetail } from '@ionic/core';
   styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+  isLoading: boolean | null = null;
+  isMoreInfoVisible: boolean;
   allPlaces: Place[];
-  selectedPlace: Place;
   @ViewChild('pageTop') pageTop;
 
-  constructor(private _placesService: PlacesService, private menuCtrl: MenuController) { }
+  constructor(
+    private _favouritePlaces: FavouritePlacesService,
+    private _placesService: PlacesService,
+    private _authService: AuthService,
+    public modalController: ModalController,
+    public alertController: AlertController,
+    private router: Router
+  ) { }
 
   ngOnInit() { 
+  }
+
+  ionViewWillEnter() {
     //fetch all places when loaded; no update for now so fetch 1 time just good enough
-    this._placesService.allPlaces.subscribe(data =>
-      {
-        if (data) {
-          this.allPlaces = data;
-          this.selectedPlace = this.allPlaces[0];
+    this.getAllPlaces();
+  }
+
+  getAllPlaces() {
+    this.isLoading = true;
+    let getSubscription = this._placesService.getPlacesAPIObservable();
+    getSubscription.subscribe(data => {
+      this.allPlaces = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            ...(e.payload.doc.data() as {})
+          } as Place;
+        });
+      this.isLoading = false;
+    }, (error) => this.isLoading = false);
+  }
+
+  addToFavourites(placeId: string) {
+    this._favouritePlaces.getFavouritePlaceAPIObservableAsData(this._authService.getUserId)
+      .subscribe(data => {
+        if (Array.isArray(data) && data.length) {
+          let favouritePlaceObject = data.pop();
+          this._favouritePlaces.addFavouritePlaceAPI(placeId, favouritePlaceObject);
+        } else {
+          this._favouritePlaces.createFavouritePlaceAPIPromise(this._authService.getUserId, placeId);
         }
-      }
-    );
+      });
+  }
+
+  presentAlert(title:string, placeId: string) {
+    this.alertController
+      .create({
+        cssClass: 'my-custom-class',
+        header: 'Adaugă la favorite',
+        // subHeader: 'Subtitle',
+        message: `Adaugă ${title} la locații favorite?`,
+        buttons: [
+          {
+            text: 'Nu',
+            role: 'cancel'
+          },
+          {
+            text: 'Da',
+            handler: () => { 
+              this.addToFavourites(placeId);
+              return true;
+            }
+          }
+        ]
+      })
+      .then(alert => {
+        alert.present();
+      });
   }
 
   onOpenMenu(){
@@ -36,8 +95,7 @@ export class DiscoverPage implements OnInit {
     console.log(event.detail);
   }
 
-  selectPlace(place: Place) {
-    this.selectedPlace = place;
-    this.pageTop.scrollToTop();
+  toggleMoreInfoVisibility() {
+    this.isMoreInfoVisible = !this.isMoreInfoVisible;
   }
 }
